2f450c8bb33c494fc58fd538b4c0b3f93de028aa
Abhishek Pandey <abhishekpandey10196@gmail.com>
2023-02-05 00:36:43 +0530

    test

--- gi.sh
+++ gi.sh
@@ -1,2 +1,3 @@
 cd example
-pod outdated | grep -i 'Amplitude'
+pod outdated | grep -i 'Amplitude' | cut -d ">" -f2 | cut -d "(" -f1 | sed 's/ //g'
+pod outdated | grep -i 'Amplitude' | cut -d "(" -f2 | cut -d ")" -f1 | sed 's/ //g'
