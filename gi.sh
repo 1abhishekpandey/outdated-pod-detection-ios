343243585448e0cb2a612def644832f850c53c89
Abhishek Pandey <abhishekpandey10196@gmail.com>
2023-02-04 20:54:08 +0530

    test

--- gi.sh
+++ gi.sh
@@ -1,3 +1,3 @@
-# number=$(gh issue list --search "fix: update Amplitude SDK to the latest version" --json number)
-# echo ${number}
+number=$(gh issue list --search "fix: update Amplitude SDK to the latest version" --json number | jq '.[].number')
+echo ${number}
 echo '[{"number":21}]' | jq '.[].number'
