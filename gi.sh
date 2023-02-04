45ae2922aed9cadc110d7005f0b22b579722e82a
Abhishek Pandey <abhishekpandey10196@gmail.com>
2023-02-04 22:49:20 +0530

    test

--- gi.sh
+++ gi.sh
@@ -1,3 +1,4 @@
 number=$(gh issue list --search "fix: update Amplitude SDK to the latest version" --json number | jq '.[].number')
 echo ${number}
 echo '[{"number":21}]' | jq '.[].number'
+gh issue create -a "1abhishekpandey" -b "Amplitude: new version available 7.2.2 -> 7.2.2 (latest version 8.14.0)" -t "fix: update Amplitude SDK to the latest version" --label "outdatedPod"
