25ef9e75c95a4ac633317b8424b4ab1f01dfd6f1
Abhishek Pandey <abhishekpandey10196@gmail.com>
2023-02-07 23:20:20 +0530

    test

--- gi.sh
+++ gi.sh
@@ -1,3 +1,9 @@
-cd example
-pod outdated | grep -i 'Amplitude' | cut -d ">" -f2 | cut -d "(" -f1 | sed 's/ //g'
-pod outdated | grep -i 'Amplitude' | cut -d "(" -f2 | cut -d ")" -f1
+
+a="$(gh issue list --search 'fix: update SDK to the latest version' --json number,title | jq '.[]')"
+# echo "'[{"number":56,"title":"fix: update Amplitude SDK to the latest version"}]' | jq '.[].title')"
+# echo $($a | jq '.[] | .title')
+echo $a 
+echo $a | jq '.title'
+echo $a | jq '.number'
+b="$($a | jq '.title')"
+echo $b
