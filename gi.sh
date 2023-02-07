81d25dea91accae387b159fe4fba151069b601b5
Abhishek Pandey <abhishekpandey10196@gmail.com>
2023-02-08 00:29:23 +0530

    test

--- gi.sh
+++ gi.sh
@@ -1,9 +1,7 @@
 
 a="$(gh issue list --search 'fix: update SDK to the latest version' --json number,title | jq '.[]')"
-# echo "'[{"number":56,"title":"fix: update Amplitude SDK to the latest version"}]' | jq '.[].title')"
-# echo $($a | jq '.[] | .title')
-echo $a 
-echo $a | jq '.title'
-echo $a | jq '.number'
-b="$($a | jq '.title')"
-echo $b
+# echo $a 
+echo $a | jq '.title' | tr -d '"'
+# echo $a | jq '.number'
+# b="$($a | jq '.title')"
+# echo [["$a | jq '.title'" == 'fix: update SDK to the latest version']]
